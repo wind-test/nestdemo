be224058c62afa2cc880dce291c60636f86c4cad
huangjitao <164173925@qq.com>
2022-07-29 14:33:54 +0800

    feat: 模板引擎结合路由跳转演示

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NewsController } from './news/news.controller';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule],
   controllers: [AppController, NewsController],
   providers: [AppService],
 })
---
+++ src/user/user.controller.ts
@@ -0,0 +1,38 @@
+/*
+ * @Title: 路由器--用户
+ * @Author: huangjitao
+ * @Date: 2022-07-29 14:09:27
+ * @Description: description of this file
+ */
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Render,
+  Query,
+  Response,
+} from '@nestjs/common';
+import { UserService } from './user.service';
+
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get()
+  @Render('user/index')
+  UserIndex(@Query() req) {
+    return {
+      username: req.username,
+    };
+  }
+
+  @Post('doAdd')
+  doAdd(@Body() body, @Response() res) {
+    if (body.username) {
+      res.redirect(`/user?username=${body.username}`);
+    } else {
+      res.redirect('/user');
+    }
+  }
+}
