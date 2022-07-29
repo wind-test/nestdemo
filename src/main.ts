19e99db634330e451df5bf4477a1cc25fce4fb6f
huangjitao <164173925@qq.com>
2022-07-29 11:50:50 +0800

    feat: 配置静态资源

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
+/*
+ * @Title: 程序主入口
+ * @Author: huangjitao
+ * @Date: 2022-06-15 17:10:58
+ * @Description: description of this file
+ */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  // app.useStaticAssets('public');
+  app.useStaticAssets(join(__dirname, '../public'), { prefix: '/static/' });
   await app.listen(3000);
 }
 bootstrap();
