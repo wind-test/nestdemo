c7bef7681c793dfca8f17fe5a3cd6d4b25fa6cf9
huangjitao <164173925@qq.com>
2022-08-03 14:53:33 +0800

    feat: Nestjs 中创建使用中间件

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,38 @@
-import { Module } from '@nestjs/common';
+/*
+ * @Title: 应用总控制器
+ * @Author: huangjitao
+ * @Date: 2022-06-15 17:10:58
+ * @Description: description of this file
+ */
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NewsController } from './news/news.controller';
 import { UserModule } from './user/user.module';
 import { UploadModule } from './upload/upload.module';
+import { LoggerMiddleware } from './middleware/logger.middleware';
 
 @Module({
   imports: [UserModule, UploadModule],
   controllers: [AppController, NewsController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      // .forRoutes('*'); //匹配所有的路由
+      // .forRoutes('user'); //匹配指定路由
+      .forRoutes(
+        { path: 'user', method: RequestMethod.ALL },
+        { path: 'user/:id', method: RequestMethod.ALL },
+        { path: 'news', method: RequestMethod.ALL },
+        { path: 'news/list', method: RequestMethod.ALL },
+      ); // 匹配多个路由
+  }
+}
---
+++ src/middleware/logger.middleware.ts
@@ -0,0 +1,15 @@
+/*
+ * @Title: 打印中间件
+ * @Author: huangjitao
+ * @Date: 2022-08-03 14:15:34
+ * @Description: 用于打印请求对象
+ */
+import { Injectable, NestMiddleware } from '@nestjs/common';
+
+@Injectable()
+export class LoggerMiddleware implements NestMiddleware {
+  use(req: any, res: any, next: () => void) {
+    console.log('打印请求对象：', req);
+    next();
+  }
+}
